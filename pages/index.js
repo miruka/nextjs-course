915bc515fe66382000b269f43f7a596794caafae
miruka <xchrismiruka@gmail.com>
2021-05-11 18:00:59 +0300

    Added Articles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
 import Head from "next/head";
-
-export default function Home() {
+import ArticleList from "../components/ArticleList";
+export default function Home({ articles }) {
   return (
     <div>
-      <head>
+      <Head>
         <title>Nabaya Technologies</title>
         <meta
           name="keywords"
           content="graphic design, branding, large format printing"
         ></meta>
-      </head>
-      <h1>Welcome to Home of Creativity</h1>
+      </Head>
+      {/* <h1>Welcome to Home of Creativity</h1> */}
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+  return {
+    props: {
+      articles,
+    },
+  };
+};
